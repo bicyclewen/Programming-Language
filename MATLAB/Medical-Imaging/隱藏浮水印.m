78d0a8aa5c92a09e4aaef604164340bc3cce1b74
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-07 21:57:43 +0800

    code

---
+++ MATLAB/Medical-Imaging/隱藏浮水印.m
@@ -0,0 +1,16 @@
+cover = imread("forest.jpg"); //載體圖
+secret = imread("p.bmp"); //浮水印 (或朴寶英???)
+
+secret = imresize(secret, [size(cover,1) size(cover,2)]); //size一樣大
+
+cover_high = bitand(cover, uint8(240));
+secret_high = bitand(secret, uint8(240));
+secret_shift = bitshift(secret_high, -4);
+
+img = cover_high + secret_shift;
+
+figure(1);
+subplot(1,2,1); imshow(cover); title("原圖");
+subplot(1,2,2); imshow(img); title("有藏圖的");
+
+imwrite(img, "img.bmp"); //匯出圖片(會跑到桌面)
