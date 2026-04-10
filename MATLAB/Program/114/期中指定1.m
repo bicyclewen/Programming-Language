f8bfbe13aa0a40ec1caf9bb2ecf7f46c4f168852
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-10 23:33:41 +0800

    code

--- MATLAB/Program/114/期中指定1.m
+++ MATLAB/Program/114/期中指定1.m
@@ -1,1 +1,10 @@
+x = 0:0.01:2;
 
+u = 2*log(60*x+1)/log(5);
+v = 3*x.*sin(cos(6*x));
+
+plot(x,u,'b'); hold on %等紅線
+plot(x,v,'r')
+
+xlabel("Distance x (mi)"); ylabel("Speed (mi/hr)");
+legend('u','v')
