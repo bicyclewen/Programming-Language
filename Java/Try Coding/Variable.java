1ff032e4ccef09903ee49209751bd201496194f4
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-10 16:57:46 +0800

    code

--- Java/Try Coding/Variable.java
+++ Java/Try Coding/Variable.java
@@ -1,10 +1,10 @@
 Variable.java
 public class Variable{
-public static void main(String[] argv){
-int i;
-i=20;
-System.out.println( "i="+i );
-}
+  public static void main(String[] argv){
+    int i;
+    i=20;
+    System.out.println( "i="+i );
+  }
 }
 
 //Java is strict-typed language，對資料型別嚴格
