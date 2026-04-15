78744bcc2e68b2cc4669a1e880f376eb1b6fb909
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-15 11:16:11 +0800

    code

--- C#/打NET作業/5-1總成績計算系統.cs
+++ C#/打NET作業/5-1總成績計算系統.cs
@@ -7,7 +7,7 @@ namespace 打NET作業5
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e) //計算按鈕
+        private void button1_Click(object sender, EventArgs e)
         {
             double general = Convert.ToDouble(textBox1.Text);
             double mid = Convert.ToDouble(textBox2.Text);
@@ -16,6 +16,21 @@ namespace 打NET作業5
             double overall = general * 0.3 + mid * 0.3 + final * 0.4;
             label5.Text = Math.Round(overall, 2).ToString();
             label5.BackColor = Color.White;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.MaxLength = 3; //輸入分數不超過三位數
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.MaxLength = 3;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.MaxLength = 3;
         }
     }
 }
