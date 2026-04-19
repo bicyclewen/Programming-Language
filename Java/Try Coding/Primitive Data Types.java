f82f35e902c1d2ad7f0653dba8ebbe52f3c3e203
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-19 14:06:33 +0800

    code

---
+++ Java/Try Coding/Primitive Data Types.java
@@ -0,0 +1,12 @@
+/*字尾標記 & printf佔位符
+int     %d 
+long    %d 字尾加L
+float   %f 字尾加F
+double  %f (預設)
+boolean %b
+char    %c
+String  %s
+*/
+byte a = 10,b = 20;
+byte c = (byte)(a+b); // 因為 byte c = a + b，java為了精確度會自動提升成int
+
