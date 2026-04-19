2f5e506fabcf760c97f95de352f1a8461791146f
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-19 14:18:19 +0800

    code
    Add example of array usage in Java with integers and strings.

---
+++ Java/Try Coding/*Reference Data Types.java
@@ -0,0 +1,9 @@
+//陣列 Array
+int[] numbers = new int[5]; //開空間 //java從0開始數 ~ 0、1、2、3、4
+String[] fruits = {"banana", "melon"};
+
+int[] data = {1, 10, 100, 1000, 10000};
+System.out.print(data.lenght); //輸出: 5
+for (int num : data) {
+    System.out.println(num);
+}
