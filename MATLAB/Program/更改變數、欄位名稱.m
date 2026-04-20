fc522219bec6ee9c7cc7b12a80f3d4dd715226f0
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-20 11:07:22 +0800

    code

--- MATLAB/Program/更改變數、欄位名稱.m
+++ MATLAB/Program/更改變數、欄位名稱.m
@@ -1,19 +1,19 @@
 %【前情提要】
 load ovariancancer.mat;
-/*whos
+%*whos
   Name        Size                Bytes  Class     Attributes
 
   grp       216x1                 28512  cell                
-  obs       216x4000            3456000  single */          
+  obs       216x4000            3456000  single *%      
 %將兩變數合成一個table
 obs2 = array2table(obs);
 obs2(:,4001)=grp;
-/*whos
+%*whos
   Name        Size                Bytes  Class     Attributes
 
   grp       216x1                 28512  cell                
   obs       216x4000            3456000  single              
-  obs2      216x4001            4499287  table */
+  obs2      216x4001            4499287  table *%
 
 %【更改欄位名稱】
 %確認欄位名稱再改名
