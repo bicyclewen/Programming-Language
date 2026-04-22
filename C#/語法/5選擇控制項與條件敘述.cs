7ff3145b63f3e57a7ec6d92be4478a65a06e70b0
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-22 10:23:55 +0800

    code

---
+++ C#/語法/5選擇控制項與條件敘述.cs
@@ -0,0 +1,9 @@
+* 程式流程結構
+  一、循序 (內定)
+  二、選擇 : if、switch ~那個菱形
+  三、重複 (aka 迴圈)
+
+選擇題:
+  單選 ~ RadioButton 控制元件
+  多選 ~ CheckBox 控制元件
+  *GroupBox:提供做分類
--- C#/語法/文字方塊控制項.cs
+++
@@ -1,3 +0,0 @@
-textBox.MaxLength = 10; //限制字元數
-PasswordChar //密碼本人在label的text裡面
-ScrollBar
