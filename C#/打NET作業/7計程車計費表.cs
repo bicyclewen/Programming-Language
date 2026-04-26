2cf73e5710db275008fa2df67074530b2878a1f3
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-26 19:00:53 +0800

    code

---
+++ C#/打NET作業/7計程車計費表.cs
@@ -0,0 +1,14 @@
+button1_Click //"計費"按鈕
+  int dist = Convert.ToInt32(textBox1.Text);
+  int fare = 90;
+
+  if (dist > 1500) {
+    int ex_dist = dist - 1500;
+    fare += ((ex_dist + 500 - 1) / 500) * 8;
+  }
+  label2.Text = (dist > 0) ? Convert.ToString(fare) : "?";
+
+button2_Click //"清除"按鈕
+  //重設
+  label2.Text = "0";
+  textBox1.Text = "";
