78bc277f846f98fa35376c50413cd17c40811958
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-29 11:00:44 +0800

    Update 5-1總成績計算系統.cs

--- C#/打NET作業/5-1總成績計算系統.cs
+++ C#/打NET作業/5-1總成績計算系統.cs
@@ -1,36 +1,17 @@
-namespace 打NET作業5
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+button1_Click //計算按鈕
+    double general = Convert.ToDouble(textBox1.Text);
+    double mid = Convert.ToDouble(textBox2.Text);
+    double final = Convert.ToDouble(textBox3.Text);
 
-        private void button1_Click(object sender, EventArgs e) //計算按鈕
-        {
-            double general = Convert.ToDouble(textBox1.Text);
-            double mid = Convert.ToDouble(textBox2.Text);
-            double final = Convert.ToDouble(textBox3.Text);
+    double overall = general * 0.3 + mid * 0.3 + final * 0.4;
+    label5.Text = Math.Round(overall, 2).ToString();
+    label5.BackColor = Color.White;
 
-            double overall = general * 0.3 + mid * 0.3 + final * 0.4;
-            label5.Text = Math.Round(overall, 2).ToString();
-            label5.BackColor = Color.White;
-        }
+textBox1_TextChanged
+    textBox1.MaxLength = 3; //輸入分數不超過三位數
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            textBox1.MaxLength = 3; //輸入分數不超過三位數
-        }
+textBox2_TextChanged
+    textBox2.MaxLength = 3;
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            textBox2.MaxLength = 3;
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-            textBox3.MaxLength = 3;
-        }
-    }
-}
+textBox3_TextChanged
+    textBox3.MaxLength = 3;
