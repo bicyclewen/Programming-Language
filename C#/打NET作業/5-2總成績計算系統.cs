9a280b57f49af6aef5fd351846fe19653823cd89
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-29 11:02:00 +0800

    Update 5-2總成績計算系統.cs

--- C#/打NET作業/5-2總成績計算系統.cs
+++ C#/打NET作業/5-2總成績計算系統.cs
@@ -1,30 +1,17 @@
-namespace 打NET作業6
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+button1_Click //計算按鈕
+    double general = Convert.ToDouble(textBox1.Text);
+    double mid = Convert.ToDouble(textBox2.Text);
+    double final = Convert.ToDouble(textBox3.Text);
 
-        private void button1_Click(object sender, EventArgs e) //計算按鈕
-        {
-            double general = Convert.ToDouble(textBox1.Text);
-            double mid = Convert.ToDouble(textBox2.Text);
-            double final = Convert.ToDouble(textBox3.Text);
+    double overall = general * 0.3 + mid * 0.3 + final * 0.4;
+    string txt = "   您的總成績 = ";
+    txt += Convert.ToString(overall);
+    MessageBox.Show(txt, "成績計算系統", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            double overall = general * 0.3 + mid * 0.3 + final * 0.4;
-            string txt = "   您的總成績 = ";
-            txt += Convert.ToString(overall);
-            MessageBox.Show(txt, "成績計算系統", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
+textBox1_TextChanged
+    textBox1.MaxLength = 3;
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            textBox1.MaxLength = 3;
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
+textBox2_TextChanged
         {
             textBox2.MaxLength = 3;
         }
