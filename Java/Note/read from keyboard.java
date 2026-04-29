03662cc05f00c35466fb7a1332f619b8cae91d08
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-29 09:01:20 +0800

    Correct main method signature in ReadString class
    Fix method signature to correctly declare parameters and exceptions.

--- Java/Note/read from keyboard.java
+++ Java/Note/read from keyboard.java
@@ -1,8 +1,16 @@
 import java.io.*; //星號表"全部" //放public前面
 public class ReadString{
-  public static void main(String[] argv throws IO Exception){
+  public static void main(throws IO Exception String[] argv){
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     // new 保留資料流空間 //緩衝字流轉換
     String str = br.readLine();
   }
 }
+
+//Convert strings to numeric types:
+//String s = "123"; String t = "12.34";
+byte b = Byte.parseByte(s);
+short c = Short.parseShort(s);
+int d = Integer.parseInt(s);
+long e = Long.parseLong(s);
+Float f = Float.parseFloat(t);
