72ccbc282762e44ad0433b6aed25ad5df017e9d4
溫子晴 <bicyclewen.1214@gmail.com>
2026-04-29 10:44:08 +0800

    Refactor button click event handlers for clarity

--- C#/打NET作業/2兩數相加.cs
+++ C#/打NET作業/2兩數相加.cs
@@ -1,25 +1,11 @@
-namespace WinFormsApp1
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+button1_Click //"Hello"按鈕
+    label3.Text = label3.Text + " " + textBox1.Text;
 
-        private void Form1_Load(object sender, EventArgs e){
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            label3.Text = label3.Text + " " + textBox1.Text;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            double x = Convert.ToDouble(textBox2.Text);
-            double y = Convert.ToDouble(textBox3.Text);
-            label6.Text = label6.Text + (x + y); //直接串接即可
-        }
-    }
-}
+button2_Click //"相加"按鈕
+    double x = Convert.ToDouble(textBox2.Text);
+    double y = Convert.ToDouble(textBox3.Text);
+    label6.Text = label6.Text + (x + y); //直接串接即可
+/*
+label3 歡迎:
+label6 sum:
+*/
