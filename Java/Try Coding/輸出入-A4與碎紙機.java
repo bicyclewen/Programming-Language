3779dd7f51d5b5531d6c05f64aeaa9d3a59ae457
溫子晴 <bicyclewen.1214@gmail.com>
2026-05-03 16:02:44 +0800

    Create 輸出入-A4與碎紙機.java

---
+++ Java/Try Coding/輸出入-A4與碎紙機.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int per = Integer.parseInt(br.readLine());
+		double A4 = 0.03*Math.pow(100,3)/25;
+		int paper = (int)(A4*per/100);
+		System.out.print(paper);
+	}
+}
