9b5aeaf3cbe1d21e4030b49c7541d5a033342af1
溫子晴 <bicyclewen.1214@gmail.com>
2026-05-10 21:56:20 +0800

    Implement functionality to list multiples within range

--- C#/打NET作業/10列出倍數.cs
+++ C#/打NET作業/10列出倍數.cs
@@ -1,2 +1,23 @@
 /*建立C#應用程式: 由 ? 到 ? (由user輸入)
 顯示其中所有 ? (由user輸入)的倍數之數字*/
+button1 //"GO!"按鈕
+try {
+    textBox4.Text = "";
+    int lower = Convert.ToInt32(textBox1.Text);
+    int upper = Convert.ToInt32(textBox2.Text);
+    int number = Convert.ToInt32(textBox3.Text);
+    int i = lower;
+    
+    //尋找首項
+    while (i%number!=0) { 
+        i++;
+    }
+    while (i<=upper) {
+        textBox4.Text += (i+"\r\n");
+        i += number;
+    }
+
+}
+catch {
+    MessageBox.Show("請輸入正確數字~");
+}
