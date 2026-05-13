ef6c8da917a0897fe14e1d5b18b08b9370d8da19
溫子晴 <bicyclewen.1214@gmail.com>
2026-05-13 11:05:33 +0800

    Update 7.cs

--- C#/打NET程設Note/7.cs
+++ C#/打NET程設Note/7.cs
@@ -1,9 +1,17 @@
 認識函數 Function
 
-修飾子 Modifiers
+修飾子 Modifiers ~ public、private
   修飾子 void 函數名稱(){
     程式敘述;
   }
-public
-private
 
+#有傳回值
+[public|private]傳回值型別 函數名稱(參數1, 參數2, ...){
+  程式敘述;
+  return值|運算式;
+}
+
+#沒有傳回值
+void 函數名稱(){
+  
+}
