190dd127295d0adce92791675a22b8eba643c2bf
溫子晴 <bicyclewen.1214@gmail.com>
2026-05-13 11:07:51 +0800

    update

--- C#/打NET作業/11階乘和.cs
+++ C#/打NET作業/11階乘和.cs
@@ -2,10 +2,10 @@
    S、e皆同為奇數或偶數，且S<=e
    若不符則警告並清空，要求使用者重新輸入 */
 button1 //"GO!"按鈕
-int S = Convert.ToInt32(textBox1.Text), E = Convert.ToInt32(textBox2.Text);
-int result2=0;
 try {
-    if (S % 2 == E % 2) { // 確定 S、E 皆為偶數或奇數
+    int S = Convert.ToInt32(textBox1.Text), E = Convert.ToInt32(textBox2.Text);
+    int result2=0;
+    if (S<=E && (E-S)%2==0) { // 確定S<=E 且 S、E 皆為偶數或奇數
         textBox3.Text = "";
         for (int i = S; i <= E; i += 2) { //第一層~算整體總和
             int result1 = 1;
@@ -17,18 +17,18 @@ try {
                 textBox3.Text += (i == S) ? (S + "! + ") : (E+"! = "+result2);
             }
             else {
-                textBox3.Text += ("("+S+"+"+(i-2)+")! + ");
+                textBox3.Text += (i+"! + ");
             }
         }
         
     }
     else {
-        DialogResult tell = MessageBox.Show("S、E 需皆為偶數或奇數", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        DialogResult tell = MessageBox.Show("S<=E，且 S、E 需皆為偶數或奇數", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
         if (tell == DialogResult.OK) {
         textBox1.Text = textBox2.Text = textBox3.Text = "";
         }
     }
 }
 catch{
-    MessageBox.Show("請輸入正確數值", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    MessageBox.Show("請輸入整數數值", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
 }
