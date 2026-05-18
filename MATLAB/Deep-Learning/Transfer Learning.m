30816641b2fb52da8bcbf37e2929f5e9e27878c3
溫子晴 <bicyclewen.1214@gmail.com>
2026-05-18 10:36:35 +0800

    Update Transfer Learning.m

--- MATLAB/Deep-Learning/Transfer Learning.m
+++ MATLAB/Deep-Learning/Transfer Learning.m
@@ -1,5 +1,7 @@
 https://www.mathworks.com/help/deeplearning/gs/get-started-with-transfer-learning.html
-透過已訓練好的模型，訓練自己的資料庫
+目標: 將上次image classification的資料用此模型訓練
+
+Transfer Learning : 透過已訓練好的模型，訓練自己的資料庫
   修改圖片大小 種類
 
   隨機性多樣性
