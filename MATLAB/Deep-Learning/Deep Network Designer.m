394fdc4f23d037ac6eaf603163e2cd240d8ee7b4
溫子晴 <bicyclewen.1214@gmail.com>
2026-05-18 09:36:26 +0800

    Create Deep Network Designer.m

---
+++ MATLAB/Deep-Learning/Deep Network Designer.m
@@ -0,0 +1,16 @@
+https://www.mathworks.com/help/deeplearning/gs/get-started-with-deep-network-designer.html
+目標: 訓練lstm神經網路
+每個序列有三個channal，且長度不一
+
+input : 
+⭣
+lstm : 
+⭣
+dropout : 
+⭣
+fullyConnected : 全連接層
+⭣
+softmax : 在分類很重要，結束變機率值(總和為1)
+
+{} 會呈現content，() 會呈現索引值
+
