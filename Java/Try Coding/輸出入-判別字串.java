f38979d044ddc885d5e2b209c6e2b86dd6add4c3
溫子晴 <bicyclewen.1214@gmail.com>
2026-05-18 08:58:42 +0800

    Add gender-based weight calculation program

---
+++ Java/Try Coding/輸出入-判別字串.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String G = br.readLine();
+		double height = Double.parseDouble(br.readLine());
+		double weight;
+		
+		if("M".equals(G)||"m".equals(G)){
+			weight=(height-80)*0.7;
+		}else if("F".equals(G)||"m".equals(G)){
+			weight=(height-70)*0.6;
+		}else{
+			System.out.print("Invalid value");
+			return;
+		}
+		System.out.printf("%.1f", weight);
+	}
+}
