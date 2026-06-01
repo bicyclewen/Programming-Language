4943e24c72f8de7e891eb766aec329820047c347
溫子晴 <bicyclewen.1214@gmail.com>
2026-06-01 11:08:45 +0800

    Update 期末指定2.m

--- MATLAB/Deep-Learning/test/期末指定2.m
+++ MATLAB/Deep-Learning/test/期末指定2.m
@@ -1,2 +1,28 @@
 https://www.mathworks.com/help/deeplearning/gs/create-simple-deep-learning-classification-network.html
+
 ~網路架構微調與自動化~
+a) 自動化計算
+  numClasses = numel(categories(imdsTrain.Labels));
+b) 增加網路架構
+  inputSize = [28 28 1];
+  numClasses = numel(categories(imdsTrain.Labels));
+  
+  layers = [
+      imageInputLayer(inputSize)
+      
+      convolution2dLayer(3,16,Padding="same")
+      batchNormalizationLayer
+      reluLayer
+      maxPooling2dLayer(2,Stride=2)
+  
+      convolution2dLayer(3,32,Padding="same")
+      batchNormalizationLayer
+      reluLayer
+      maxPooling2dLayer(2,Stride=2)
+      
+      convolution2dLayer(3,64,Padding="same")
+      batchNormalizationLayer
+      reluLayer
+  
+      fullyConnectedLayer(numClasses)
+      softmaxLayer];
