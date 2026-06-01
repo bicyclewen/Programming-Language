cbe6dac620a24e22f042d48ddaa9b098a68b8305
溫子晴 <bicyclewen.1214@gmail.com>
2026-06-01 10:08:07 +0800

    Update trainingOptions.m

--- MATLAB/Deep-Learning/trainingOptions.m
+++ MATLAB/Deep-Learning/trainingOptions.m
@@ -1,6 +1,8 @@
 https://www.mathworks.com/help/deeplearning/ref/trainingoptions.html
 
 EX1 Specify Training Option
-piecewise 片段連續(片段與片段間不連續)
+  piecewise 片段連續(片段與片段間不連續)
 
 EX2 Monitor Deep Learning Training Progress
+
+訓練完，如何看到訓練結果
