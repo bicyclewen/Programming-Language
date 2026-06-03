a62c15cdc65aff13787e5584966bcc4f3bc42a2c
溫子晴 <bicyclewen.1214@gmail.com>
2026-06-03 11:30:34 +0800

    Add search feature to document processor demo
    Implement search functionality in demo.cs to highlight search results.

---
+++ C#/打NET作業/16文件處理器-別人demo.cs
@@ -0,0 +1,18 @@
+/*功能四: 搜尋
+這個寫法像一般文件、pdf閱讀器等，一個一個框出搜尋之項目 */
+
+//button4_Click 搜尋按鈕
+string keywprd = textBox3.Text;
+int currentpos = 0;
+int pos; //字元位址
+if(pos != -1){
+  textBox6.Focus();
+  textBox6.SelectionStart = pos;
+  textBox6.SelectionLength = keyword.Length;
+  currentpos = pos + 1; //下一個搜尋到的字元
+}else{
+  MessageBox.Show("搜尋完畢");
+  currentpos = 0;
+  return;
+}
+//textBox6是最初輸入的那些字串，textBox3是搜尋框
