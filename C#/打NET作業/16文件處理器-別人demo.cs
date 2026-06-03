7b80627e75cefa6be226162a2e8b75204b62f305
溫子晴 <bicyclewen.1214@gmail.com>
2026-06-03 11:37:27 +0800

    Fix typo and update search functionality

--- C#/打NET作業/16文件處理器-別人demo.cs
+++ C#/打NET作業/16文件處理器-別人demo.cs
@@ -2,9 +2,9 @@
 這個寫法像一般文件、pdf閱讀器等，一個一個框出搜尋之項目 */
 
 //button4_Click 搜尋按鈕
-string keywprd = textBox3.Text;
+string keyword = textBox3.Text;
 int currentpos = 0;
-int pos; //字元位址
+int pos = textBox4.Text.IndexOf(keyword.currentpos); //字元位址
 if(pos != -1){
   textBox6.Focus();
   textBox6.SelectionStart = pos;
