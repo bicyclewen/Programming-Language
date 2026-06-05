3d14ac2a3ee53536b233f70997ea74b629e6abe6
溫子晴 <bicyclewen.1214@gmail.com>
2026-06-05 14:10:57 +0800

    Update 期末指定4.m

--- MATLAB/Deep-Learning/test/期末指定4.m
+++ MATLAB/Deep-Learning/test/期末指定4.m
@@ -1,1 +1,20 @@
+https://www.mathworks.com/help/deeplearning/gs/classify-image-using-pretrained-network.html
+~預訓練模型~
+a) 更換網路模型
+[net,classNames] = imagePretrainedNetwork("squeezenet");
 
+b) 準備照片
+inputSize = net.Layers(1).InputSize;
+image = {"llama.jpg","football.jpg","sherlock.jpg"};
+
+c) 讀取並調整照片
+for i=1:3
+  I = imread(image{i});
+  I = imresize(I,inputSize(1:2));
+  
+  scores = predict(net,single(I));
+  label = scores2label(scores,classNames);
+  figure
+  imshow(I)
+  title(string(label))
+end
