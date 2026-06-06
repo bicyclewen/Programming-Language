a5ea3eb29ba6e1a50a12edcf87fd5d7988e54542
溫子晴 <bicyclewen.1214@gmail.com>
2026-06-06 16:31:34 +0800

    Update 期末指定5.m

--- MATLAB/Deep-Learning/test/期末指定5.m
+++ MATLAB/Deep-Learning/test/期末指定5.m
@@ -1,1 +1,15 @@
+https://www.mathworks.com/help/deeplearning/gs/create-simple-deep-learning-classification-network.html
+https://www.mathworks.com/help/deeplearning/ug/create-simple-deep-learning-network-for-classification.html?s_tid=srchtitle_support_results_1_create%2520simple%2520deep%2520learning%2520classification%2520network
 
+~confusion matrix~
+a) 切割資料
+[imdsTrain,imdsValidation, imdsTest] = splitEachLabel(imds,0.7,0.15,0.15,"randomized");
+
+b) 
+scores = minibatchpredict(net,imdsTest);
+YTest = scores2label(scores,classNames);
+TTest = imdsTest.Labels;
+accuracy = mean(YTest == TTest)
+
+figure;
+confusionchart(TTest,YTest);
