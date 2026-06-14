011136c57fbacc413d78c15cf1e13d6a8ac72e72
溫子晴 <bicyclewen.1214@gmail.com>
2026-06-14 16:59:07 +0800

    Update comments and training options in script

--- MATLAB/Deep-Learning/test/期末指定1.m
+++ MATLAB/Deep-Learning/test/期末指定1.m
@@ -1,12 +1,13 @@
 https://www.mathworks.com/help/deeplearning/gs/create-simple-deep-learning-classification-network.html
 
 ~基礎模型訓練與參數配置~
-a) Load Data 分割資料
-[imdsTrain,imdsValidation,imdsTest] = splitEachLabel(imds,0.7,0.1,0.2,"randomized");
+a) 資料分割
+[imdsTrain,imdsValidation,imdsTest] = splitEachLabel(imds,0.7,0.1,"randomized");
+剩餘之0.2會自動變為imdsTest
 b) Specify Training Option 訓練參數
 options = trainingOptions("adam", ... %solver從sgdm改成adam
                           MaxEpochs=3, ... %最大訓練輪數從4改成3
-c) Test Neural Network
+c) 修改資料集名稱
 accuracy = testnet(net,imdsTest,"accuracy") %確認準確率函數
 
 按Run
